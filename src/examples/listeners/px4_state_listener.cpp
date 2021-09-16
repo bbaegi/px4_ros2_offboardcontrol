e2b42b632b3258fe0e9289892f3107fc08251f5d
bbaegi <devbbaegi@gmail.com>
2021-09-16 18:24:51 +0900

    add clock in Serial RX/TX

--- src/examples/listeners/px4_state_listener.cpp
+++ src/examples/listeners/px4_state_listener.cpp
@@ -40,6 +40,8 @@
 
 #include <iostream>
 #include <memory>
+#include <unistd.h>
+#include <time.h>
 
 #include <rclcpp/rclcpp.hpp>
 #include <px4_msgs/msg/vehicle_status.hpp> // for Mode/Arming status
@@ -55,7 +57,15 @@ using namespace std;
 using std::placeholders::_1;
 
 std::string sendmsg = "";
+
+bool tEnd = false;
+std::string readData = "";
+bool read_parse = false;
+
+
 float position_x = 0;
+unsigned char px4_mode;
+float bat_remain;
 
 /**
  * @brief PX4 State uORB topic data callback
@@ -82,12 +92,14 @@ private:
     {
         /*
         std::cout << "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
-        std::cout << "RECEIVED PX4 Position DATA"   << std::endl;
+        */
+        //std::cout << "RECEIVED PX4 Position DATA"   << std::endl;
+        /*
         std::cout << "============================="   << std::endl;
         std::cout << "ts: "          << msg->timestamp    << std::endl;
-        */
+
         std::cout << "Local Position x: " << msg->x  << std::endl;
-        /*
+
         std::cout << "Local Position y: " << msg->y  << std::endl;
         std::cout << "Local Postiion z: " << msg->z  << std::endl;
         std::cout << "Yaw: " << msg->heading  << std::endl;
@@ -102,19 +114,27 @@ private:
 
     void status_callback(const px4_msgs::msg::VehicleStatus::UniquePtr msg)
     {
-        std::cout << "RECEIVED PX4 STATE DATA"   << std::endl;
+        //std::cout << "RECEIVED PX4 STATE DATA"   << std::endl;
+        /*
         std::cout << "============================="   << std::endl;
         std::cout << "ts: "          << msg->timestamp    << std::endl;
         std::cout << "Mode: " << msg->nav_state  << std::endl;
         std::cout << "Arming: " << msg->arming_state  << std::endl;
+        */
+        px4_mode = msg->nav_state;
+        //sendmsg = "Mode : " + std::to_string(msg->nav_state);
     }
 
     void battery_callback(const px4_msgs::msg::BatteryStatus::UniquePtr msg)
     {
-        std::cout << "RECEIVED PX4 Battery DATA"   << std::endl;
+        //std::cout << "RECEIVED PX4 Battery DATA"   << std::endl;
+        /*
         std::cout << "============================="   << std::endl;
         std::cout << "ts: "          << msg->timestamp    << std::endl;
         std::cout << "Battery Remaining: " << msg->remaining  << std::endl;
+        */
+        bat_remain = msg->remaining;
+        //sendmsg = "Battery remaining : " + std::to_string(msg->remaining);
     }
 
 
@@ -127,18 +147,67 @@ void callback_out(){
     std::cout << position_x << std::endl;
 }
 
-/*
-void Serial_On(){
-    while(true){
+
+void Serial_TX(){
+    while(!tEnd){
         // Create serial port object and open serial port
         SerialPort serialPort("/dev/ttyUSB0", BaudRate::B_57600);
         // Use SerialPort serialPort("/dev/ttyACM0", 13000); instead if you want to provide a custom baud rate
         serialPort.SetTimeout(0); // Block when reading until any data is received
+
         serialPort.Open();
 
+        // Read some data back (will block until at least 1 byte is received due to the SetTimeout(-1) call above)
+        std::string readData_tmp;
+        serialPort.Read(readData_tmp);
+
+        if (!readData_tmp.empty()){
+            readData += readData_tmp;
+            read_parse = true;
+        }
+        else
+        {
+            if(read_parse)
+            {
+                clock_gettime(CLOCK_MONOTONIC, &begin);
+                read_parse = false;
+            }
+            else
+            {
+                clock_gettime(CLOCK_MONOTONIC, &end);
+                long time_err = ((end.tv_sec - begin.tv_sec) + (end.tv_nsec - begin.tv_nsec))/1000000; //MilliSec
+
+                if (time_err > 500)
+                {
+                    std::cout << time_err << std::endl;
+                    std::cout << readData << std::endl;
+                    readData = "";
+                }
+            }
+
+        }
+
         // Write some ASCII data
+        sendmsg = "position x : " + std::to_string(position_x) + " / Bat : "  + std::to_string(bat_remain) + "\n";
         serialPort.Write(sendmsg);
 
+        //delay
+        //std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
+        // Close the serial portendl;
+        //serialPort.Close();
+    }
+}
+
+/*
+void Serial_RX(){
+    while(!tEnd){
+        // Create serial port object and open serial port
+        SerialPort serialPort("/dev/ttyUSB0", BaudRate::B_57600);
+        // Use SerialPort serialPort("/dev/ttyACM0", 13000); instead if you want to provide a custom baud rate
+        serialPort.SetTimeout(10); // Block when reading until any data is received
+
+        serialPort.Open();
 
         // Read some data back (will block until at least 1 byte is received due to the SetTimeout(-1) call above)
         std::string readData;
@@ -148,9 +217,6 @@ void Serial_On(){
             std::cout << readData << std::endl;
         }
 
-        //delay
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-
         // Close the serial portendl;
         //serialPort.Close();
     }
@@ -158,27 +224,33 @@ void Serial_On(){
 */
 
 
+
 int main(int argc, char *argv[])
 {
+    struct timespec begin, end;
+    clock_gettime(CLOCK_MONOTONIC,&begin);
     std::cout << "Starting px4_state_listener node..." << std::endl;
     setvbuf(stdout, NULL, _IONBF, BUFSIZ);
     rclcpp::init(argc, argv);
-    rclcpp::WallRate loop_rate(1);
+    //rclcpp::WallRate loop_rate(1);
 
+    std::thread t1(Serial_TX);
+    //std::thread t2(Serial_RX);
 
+    /*
     while(rclcpp::ok())
     {
         rclcpp::spin_some(std::make_shared<PX4StateListener>());
         loop_rate.sleep();
-        callback_out();
-
     }
-    //rclcpp::spin(std::make_shared<PX4StateListener>());
+    */
 
-    //std::thread t1(Serial_On);
+    rclcpp::spin(std::make_shared<PX4StateListener>());
 
     //t1.join();
+    //t2.join();
 
     rclcpp::shutdown();
+    tEnd = true;
     return 0;
 }
